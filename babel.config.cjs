2a3addc7eb5735ef91993fe8ee4e0a2cf3b8afe4
SrikarVanaparthy <svanaparthy@anergroup.com>
2025-01-17 14:17:20 +0530

    Remove all files from prod branch

--- babel.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: [
-      '@babel/preset-react',
-      '@babel/preset-env',  // Ensure it can handle ES modules
-    ],
-    plugins: [
-      '@babel/plugin-transform-modules-commonjs',  // Optional: Transforms ESM into CommonJS
-    ],
-  };
