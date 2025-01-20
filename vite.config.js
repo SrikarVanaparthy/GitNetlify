1400afa68569ee4269eb97d0f67d9a9e461cebe4
SrikarVanaparthy <svanaparthy@anergroup.com>
2025-01-20 15:26:30 +0530

    modifed github workflow file

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig } from 'vite'; // Ensure this is imported
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
